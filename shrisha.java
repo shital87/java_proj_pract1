907472a24a2503cae5db4d9834d4213a1374dcfb
shital <shitalpadwal87@gmail.com>
2023-02-16 18:29:08 +0530

    bug fix

--- shrisha.java
+++ shrisha.java
@@ -2,5 +2,6 @@ public class shrisha {
     public static void main(String[] args) {
         System.out.println("shrisha file updated");
         System.out.println("shital santosh");
+        System.out.println("shital santosh1");
     }
 }
